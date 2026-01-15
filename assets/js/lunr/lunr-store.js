5bdad0d12f53a6a39ea9eed75c23576e5e3e7f22
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-15 23:08:59 +0000

    update [ci skip]

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,103 @@
+var store = [{
+        "title": "Announcement_1",
+        "excerpt":"A simple inline announcement.  ","categories": [],
+        "tags": [],
+        "url": "/news/announcement_1/",
+        "teaser": null
+      },{
+        "title": "A long announcement with details",
+        "excerpt":"Announcements and news can be much longer than just quick inline posts. In fact, they can have all the features available for the standard blog posts. See below. Jean shorts raw denim Vice normcore, art party High Life PBR skateboard stumptown vinyl kitsch. Four loko meh 8-bit, tousled banh mi...","categories": [],
+        "tags": [],
+        "url": "/news/announcement_2/",
+        "teaser": null
+      },{
+        "title": "Announcement_3",
+        "excerpt":"A simple inline announcement with Markdown emoji! :sparkles: :smile:  ","categories": [],
+        "tags": [],
+        "url": "/news/announcement_3/",
+        "teaser": null
+      },{
+        "title": "a post with formatting and links",
+        "excerpt":"Jean shorts raw denim Vice normcore, art party High Life PBR skateboard stumptown vinyl kitsch. Four loko meh 8-bit, tousled banh mi tilde forage Schlitz dreamcatcher twee 3 wolf moon. Chambray asymmetrical paleo salvia, sartorial umami four loko master cleanse drinking vinegar brunch. Pinterest DIY authentic Schlitz, hoodie Intelligentsia butcher...","categories": ["sample-posts"],
+        "tags": ["formatting","links"],
+        "url": "/blog/2015/formatting-and-links/",
+        "teaser": null
+      },{
+        "title": "a post with images",
+        "excerpt":"This is an example post with image galleries. A simple, elegant caption looks good between image rows, after each row, or doesn't have to be there at all. Images can be made zoomable. Simply add data-zoomable to &lt;img&gt; tags that you want to make zoomable. The rest of the images...","categories": ["sample-posts"],
+        "tags": ["formatting","images"],
+        "url": "/blog/2015/images/",
+        "teaser": null
+      },{
+        "title": "a post with code",
+        "excerpt":"This theme implements a built-in Jekyll feature, the use of Rouge, for syntax highlighting. It supports more than 100 languages. This example is in C++. All you have to do is wrap your code in a liquid tag: {% highlight c++ linenos %} code code code {% endhighlight %} The...","categories": ["sample-posts"],
+        "tags": ["formatting","code"],
+        "url": "/blog/2015/code/",
+        "teaser": null
+      },{
+        "title": "a post with math",
+        "excerpt":"This theme supports rendering beautiful math in inline and display modes using MathJax 3 engine. You just need to surround your math expression with $$, like $$ E = mc^2 $$. If you leave it inside a paragraph, it will produce an inline expression, just like \\(E = mc^2\\). To...","categories": ["sample-posts"],
+        "tags": ["formatting","math"],
+        "url": "/blog/2015/math/",
+        "teaser": null
+      },{
+        "title": "a post with comments",
+        "excerpt":"This post shows how to add DISQUS comments.  ","categories": ["sample-posts","external-services"],
+        "tags": [],
+        "url": "/blog/2015/comments/",
+        "teaser": null
+      },{
+        "title": "a post with github metadata",
+        "excerpt":"A sample blog page that demonstrates the accessing of github meta data. What does Github-MetaData do? Propagates the site.github namespace with repository metadata Setting site variables : site.title site.description site.url site.baseurl Accessing the metadata - duh. Generating edittable links. Additional Reading If you’re recieving incorrect/missing data, you may need to...","categories": ["sample-posts","external-services"],
+        "tags": [],
+        "url": "/blog/2020/github-metadata/",
+        "teaser": null
+      },{
+        "title": "a distill-style blog post",
+        "excerpt":"NOTE: Citations, footnotes, and code blocks do not display correctly in the dark mode since distill does not support the dark mode by default. If you are interested in correctly adding dark mode support for distill, please open a discussion and let us know. Equations This theme supports rendering beautiful...","categories": [],
+        "tags": [],
+        "url": "/blog/2021/distill/",
+        "teaser": null
+      },{
+        "title": "Media demo: image + YouTube",
+        "excerpt":"Here’s a quick demo of adding media to a post.   Image   Inline Markdown image:      Or using the figure include (adds optional caption):              Sample caption      YouTube embed   Use the responsive iframe pattern:         Replace the URL with your video’s https://www.youtube.com/embed/&lt;id&gt;.  ","categories": ["blog"],
+        "tags": ["media","demo"],
+        "url": "/blog/2026/media-demo/",
+        "teaser": null
+      },{
+        "title": "project 1",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["work"],
+        "tags": [],
+        "url": "/projects/1_project/",
+        "teaser": null
+      },{
+        "title": "project 2",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["work"],
+        "tags": [],
+        "url": "/projects/2_project/",
+        "teaser": null
+      },{
+        "title": "project 3",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["work"],
+        "tags": [],
+        "url": "/projects/3_project/",
+        "teaser": null
+      },{
+        "title": "project 4",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["fun"],
+        "tags": [],
+        "url": "/projects/4_project/",
+        "teaser": null
+      },{
+        "title": "project 5",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["fun"],
+        "tags": [],
+        "url": "/projects/5_project/",
+        "teaser": null
+      },{
+        "title": "project 6",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["fun"],
+        "tags": [],
+        "url": "/projects/6_project/",
+        "teaser": null
+      }]
