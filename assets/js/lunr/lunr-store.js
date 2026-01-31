0945eb581cfec86fee91d8ee82b880c7c0792366
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 07:22:07 +0000

    remoce place holders and add cv [ci skip]

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Announcement_1",
+        "excerpt":"A simple inline announcement.  ","categories": [],
+        "tags": [],
+        "url": "/news/announcement_1/",
+        "teaser": null
+      },{
+        "title": "A long announcement with details",
+        "excerpt":"Announcements and news can be much longer than just quick inline posts. In fact, they can have all the features available for the standard blog posts. See below. Jean shorts raw denim Vice normcore, art party High Life PBR skateboard stumptown vinyl kitsch. Four loko meh 8-bit, tousled banh mi...","categories": [],
+        "tags": [],
+        "url": "/news/announcement_2/",
+        "teaser": null
+      },{
+        "title": "Announcement_3",
+        "excerpt":"A simple inline announcement with Markdown emoji! :sparkles: :smile:  ","categories": [],
+        "tags": [],
+        "url": "/news/announcement_3/",
+        "teaser": null
+      },{
+        "title": "project 1",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["work"],
+        "tags": [],
+        "url": "/projects/1_project/",
+        "teaser": null
+      },{
+        "title": "project 2",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["work"],
+        "tags": [],
+        "url": "/projects/2_project/",
+        "teaser": null
+      },{
+        "title": "project 3",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["work"],
+        "tags": [],
+        "url": "/projects/3_project/",
+        "teaser": null
+      },{
+        "title": "project 4",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["fun"],
+        "tags": [],
+        "url": "/projects/4_project/",
+        "teaser": null
+      },{
+        "title": "project 5",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["fun"],
+        "tags": [],
+        "url": "/projects/5_project/",
+        "teaser": null
+      },{
+        "title": "project 6",
+        "excerpt":"Every project has a beautiful feature showcase page. It’s easy to include images in a flexible 3-column grid format. Make your photos 1/3, 2/3, or full width. To give your project a background in the portfolio page, just add the img tag to the front matter like so: --- layout:...","categories": ["fun"],
+        "tags": [],
+        "url": "/projects/6_project/",
+        "teaser": null
+      }]
